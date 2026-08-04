05440a46a1337b17d342a8cf1570b8e58ac26801
carygrant <grodolfo@equicomsavings.com.ph>
2026-08-04 09:21:15 +0800

    1st commit

---
+++ SwishSystems.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SwishSystems.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
